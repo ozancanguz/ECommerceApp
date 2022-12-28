b6e888da250c098081418e72f29a3cf4925619aa
Ozan <ozancangu24@gmail.com>
2022-12-28 18:49:37 +0300

    -product database modified for favorites

--- app/src/main/java/com/ozancanguz/e_commerceapp/data/db/ProductsDatabase.kt
+++ app/src/main/java/com/ozancanguz/e_commerceapp/data/db/ProductsDatabase.kt
@@ -3,11 +3,12 @@ package com.ozancanguz.e_commerceapp.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.ozancanguz.e_commerceapp.data.db.entities.FavoritesEntity
 import com.ozancanguz.e_commerceapp.data.db.entities.ProductEntity
 
 
 @Database(
-    entities = [ProductEntity::class],
+    entities = [ProductEntity::class,FavoritesEntity::class],
     version = 1,
     exportSchema = false,
 )
