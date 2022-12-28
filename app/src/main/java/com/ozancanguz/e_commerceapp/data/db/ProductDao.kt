7e3294d4f8391b8c86c6a1ccd5979adccdb7c2a3
Ozan <ozancangu24@gmail.com>
2022-12-28 18:48:28 +0300

    -dao class modified for favorites

--- app/src/main/java/com/ozancanguz/e_commerceapp/data/db/ProductDao.kt
+++ app/src/main/java/com/ozancanguz/e_commerceapp/data/db/ProductDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.ozancanguz.e_commerceapp.data.db.entities.FavoritesEntity
 import com.ozancanguz.e_commerceapp.data.db.entities.ProductEntity
 import com.ozancanguz.e_commerceapp.data.model.Products
 import kotlinx.coroutines.flow.Flow
@@ -14,11 +15,20 @@ import kotlinx.coroutines.flow.Flow
 interface ProductDao {
 
 
+    // all products
     @Query("select * from product_table order by id asc")
     fun ListAllProducts(): Flow<List<ProductEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertProduct(productEntity:ProductEntity)
+
+
+    // fav products
+    @Query("select* from favorites_table order by id asc")
+    fun listallFavorites():Flow<List<FavoritesEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavProduct(favoritesEntity: FavoritesEntity)
 
 
 
