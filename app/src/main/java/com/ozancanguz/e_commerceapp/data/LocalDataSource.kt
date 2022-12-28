cd14d370b9787933452a5e0ab9315636e1055f5f
Ozan <ozancangu24@gmail.com>
2022-12-28 17:28:55 +0300

    -local data source functions created

---
+++ app/src/main/java/com/ozancanguz/e_commerceapp/data/LocalDataSource.kt
@@ -0,0 +1,22 @@
+package com.ozancanguz.e_commerceapp.data
+
+import com.ozancanguz.e_commerceapp.data.db.ProductDao
+import com.ozancanguz.e_commerceapp.data.db.entities.ProductEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource@Inject constructor(private val productDao: ProductDao) {
+
+
+    fun listAllProducts(): Flow<List<ProductEntity>> {
+        return productDao.ListAllProducts()
+    }
+
+
+    suspend fun insertProduct(productEntity: ProductEntity){
+        return productDao.insertProduct(productEntity)
+
+    }
+
+
+}
