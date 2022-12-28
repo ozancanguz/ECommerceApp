6e4b29764e951d1211bb2d3eb1e363e80374e4a2
Ozan <ozancangu24@gmail.com>
2022-12-28 12:09:35 +0300

    -navigation implemented

--- app/src/main/java/com/ozancanguz/e_commerceapp/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/e_commerceapp/ui/MainActivity.kt
@@ -1,14 +1,76 @@
 package com.ozancanguz.e_commerceapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ozancanguz.e_commerceapp.R
+import com.ozancanguz.e_commerceapp.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        FirebaseApp.initializeApp(this)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.productListFragment,
+                R.id.favoriteFragment,
+
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // for custom toast
+        //var layout=layoutInflater.inflate(R.layout.custom_toast,ll)
+
 
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.signoutmenu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.signoutmenu){
+            auth.signOut()
+            val intent= Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+           Toast.makeText(this,"You sign out !",Toast.LENGTH_LONG).show()
+
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
