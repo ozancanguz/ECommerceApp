d44409c8e21786cae11fe5e818e9bcdac6d1be6a
Ozan <ozancangu24@gmail.com>
2022-12-28 18:51:49 +0300

    -local data source modified for fav entities

--- app/src/main/java/com/ozancanguz/e_commerceapp/data/localdatasource/LocalDataSource.kt
+++ app/src/main/java/com/ozancanguz/e_commerceapp/data/localdatasource/LocalDataSource.kt
@@ -1,6 +1,8 @@
 package com.ozancanguz.e_commerceapp.data.localdatasource
 
+import androidx.lifecycle.LiveData
 import com.ozancanguz.e_commerceapp.data.db.ProductDao
+import com.ozancanguz.e_commerceapp.data.db.entities.FavoritesEntity
 import com.ozancanguz.e_commerceapp.data.db.entities.ProductEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -8,6 +10,7 @@ import javax.inject.Inject
 class LocalDataSource@Inject constructor(private val productDao: ProductDao) {
 
 
+    // for all product
     fun listAllProducts(): Flow<List<ProductEntity>> {
         return productDao.ListAllProducts()
     }
@@ -17,6 +20,19 @@ class LocalDataSource@Inject constructor(private val productDao: ProductDao) {
         return productDao.insertProduct(productEntity)
 
     }
+
+    // -----------------------
+    // for favorites product
+
+    fun listAllFavoritesProducts():Flow<List<FavoritesEntity>> {
+        return productDao.listallFavorites()
+    }
+
+    suspend fun insertFavorites(favoritesEntity: FavoritesEntity){
+        return productDao.insertFavProduct(favoritesEntity)
+    }
+
+
 
 
 }
