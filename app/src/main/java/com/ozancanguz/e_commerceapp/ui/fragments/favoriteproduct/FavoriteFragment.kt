858256d53502da15d2515bafed223081dd1cbbad
Ozan <ozancangu24@gmail.com>
2022-12-28 20:01:04 +0300

    -delete menu created on fav screen

--- app/src/main/java/com/ozancanguz/e_commerceapp/ui/fragments/favoriteproduct/FavoriteFragment.kt
+++ app/src/main/java/com/ozancanguz/e_commerceapp/ui/fragments/favoriteproduct/FavoriteFragment.kt
@@ -1,10 +1,8 @@
 package com.ozancanguz.e_commerceapp.ui.fragments.favoriteproduct
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -35,7 +33,13 @@ class FavoriteFragment : Fragment() {
         binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
         binding.recyclerView.adapter=favoriteListAdapter
 
+        // update ui
         observeLiveData()
+
+        // set menu
+
+        setHasOptionsMenu(true)
+
 
 
 
@@ -47,6 +51,14 @@ class FavoriteFragment : Fragment() {
                   favoriteListAdapter.setData(it)
         })
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        inflater.inflate(R.menu.deleteallmenu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
 
 
 }
