5bf23d063d61ce8b6aca5c518df2e9ae7c0d600a
Ozan <ozancangu24@gmail.com>
2022-12-28 11:26:40 +0300

    -first commit

---
+++ app/src/main/java/com/ozancanguz/e_commerceapp/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ozancanguz.e_commerceapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ozancanguz.e_commerceapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/ozancanguz/e_commerceapp/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.ozancanguz.e_commerceapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ozancanguz.e_commerceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
