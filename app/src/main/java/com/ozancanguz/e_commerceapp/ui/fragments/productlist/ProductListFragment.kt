d18d9fa5e2b927374765b1a376c093b8fe10b8df
Ozan <ozancangu24@gmail.com>
2022-12-28 12:31:53 +0300

    -data test implemented in productlist fragment

--- app/src/main/java/com/ozancanguz/e_commerceapp/ui/fragments/productlist/ProductListFragment.kt
+++ app/src/main/java/com/ozancanguz/e_commerceapp/ui/fragments/productlist/ProductListFragment.kt
@@ -1,14 +1,18 @@
 package com.ozancanguz.e_commerceapp.ui.fragments.productlist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.ozancanguz.e_commerceapp.R
 import com.ozancanguz.e_commerceapp.databinding.FragmentFavoriteBinding
 import com.ozancanguz.e_commerceapp.databinding.FragmentProductListBinding
+import com.ozancanguz.e_commerceapp.viewmodel.ProductViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +20,8 @@ class ProductListFragment : Fragment() {
     private var _binding: FragmentProductListBinding? = null
 
     private val binding get() = _binding!!
+
+    private val productviewmodel:ProductViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -26,9 +32,18 @@ class ProductListFragment : Fragment() {
         val view = binding.root
 
 
-
+        // observeLiveData
+        observeLiveData()
 
         return view
+    }
+
+    private fun observeLiveData() {
+        productviewmodel.requestProductData()
+        productviewmodel.productList.observe(viewLifecycleOwner, Observer { products ->
+            Log.d("list",""+products)
+
+        })
     }
 
 
